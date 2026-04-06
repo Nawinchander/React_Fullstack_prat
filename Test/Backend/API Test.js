24c2f0a1b1cf5a45fa1d03aa8502f02e5bba9bf5
Nawin chander <nawinnapzy289@gmail.com>
2026-04-06 21:03:22 +0530

    add

--- Test/Backend/API Test.js
+++ Test/Backend/API Test.js
@@ -1,4 +1,16 @@
-/// api test
+/// api test users
+
+const request = require("supertest");
+const app = require("../server");
+
+test("GET /api/users", async () => {
+  const res = await request(app).get("/api/users");
+
+  expect(res.statusCode).toBe(200);
+  expect(Array.isArray(res.body)).toBe(true);
+});
+
+
 
 
 
