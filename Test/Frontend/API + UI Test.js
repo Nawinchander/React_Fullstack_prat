7e1fbe612796d87f4205751c756b189ae9414e9f
Nawin chander <nawinnapzy289@gmail.com>
2026-04-06 20:59:37 +0530

    add code

--- Test/Frontend/API + UI Test.js
+++ Test/Frontend/API + UI Test.js
@@ -1,3 +1,15 @@
 //// API + UI Test (UserList)
 
 
+import { render, screen } from "@testing-library/react";
+import UserList from "./UserList";
+
+test("renders users", () => {
+  const mockUsers = [{ id: 1, name: "Nawin" }];
+
+  render(<UserList users={mockUsers} />);
+
+  expect(screen.getByText("Nawin")).toBeInTheDocument();
+});
+
+
