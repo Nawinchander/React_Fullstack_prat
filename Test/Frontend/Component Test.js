f09e832ab097c630afdb6ae15314ead692baa693
Nawin chander <nawinnapzy289@gmail.com>
2026-04-06 20:58:43 +0530

    add

--- Test/Frontend/Component Test.js
+++ Test/Frontend/Component Test.js
@@ -1,2 +1,11 @@
 /// Component Test (Header)
 
+// Header.test.jsx
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+
+test("renders title", () => {
+  render(<Header title="Full Stack App" />);
+  expect(screen.getByText("Full Stack App")).toBeInTheDocument();
+});
+
