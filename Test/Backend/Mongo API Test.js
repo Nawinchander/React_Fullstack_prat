7f39e8638f3ae483dbdaaf32972bdbb5fc22669e
Nawin chander <nawinnapzy289@gmail.com>
2026-04-06 21:04:29 +0530

    add

--- Test/Backend/Mongo API Test.js
+++ Test/Backend/Mongo API Test.js
@@ -1,3 +1,13 @@
 //// Mongo API Test
 
+test("POST /api/tasks", async () => {
+  const res = await request(app)
+    .post("/api/tasks")
+    .send({ title: "Test Task" });
+
+  expect(res.statusCode).toBe(200);
+  expect(res.body.title).toBe("Test Task");
+});
+
+
 
