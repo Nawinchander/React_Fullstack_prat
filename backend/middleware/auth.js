7a8ccc794f9b474f102284f909cceb8a2a98475b
Nawin chander <nawinnapzy289@gmail.com>
2026-04-07 21:02:38 +0530

    add

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,3 +2,22 @@
 
 /// User → Login → JWT Token → Stored in Frontend → Sent in Headers → Protected APIs
 
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) return res.status(401).json({ message: "No token" });
+
+  try {
+    const decoded = jwt.verify(token, "SECRET_KEY");
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(403).json({ message: "Invalid token" });
+  }
+};
+
+
+
+
