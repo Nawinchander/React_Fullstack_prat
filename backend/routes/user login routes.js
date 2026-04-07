9c1564b3bce2626d72df26d171b17e98ea5a1363
Nawin chander <nawinnapzy289@gmail.com>
2026-04-07 21:05:32 +0530

    code

--- backend/routes/user login routes.js
+++ backend/routes/user login routes.js
@@ -1,2 +1,25 @@
 /// user login routes
 
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const db = require("../config/mysql");
+
+const router = express.Router();
+
+router.post("/login", (req, res) => {
+  const { name } = req.body;
+
+  db.query("SELECT * FROM users WHERE name = ?", [name], async (err, result) => {
+    if (result.length === 0) return res.status(404).json({ msg: "User not found" });
+
+    const token = jwt.sign({ id: result[0].id }, "SECRET_KEY", {
+      expiresIn: "1h",
+    });
+
+    res.json({ token });
+  });
+});
+
+module.exports = router;
+
